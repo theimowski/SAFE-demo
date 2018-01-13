912d09ab35308a445ab5e14c3f6d3d630e105114
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-01-13 15:12:08 +0100

    add voting to server side

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,4 +1,5 @@
-﻿open System.IO
+﻿open System.Collections.Concurrent
+open System.IO
 open System.Net
 
 open Suave
@@ -7,6 +8,7 @@ open Suave.Operators
 open Fable.Remoting.Suave
 
 open Shared
+open System.Collections.Concurrent
 
 let clientPath = Path.Combine("..","Client") |> Path.GetFullPath 
 let port = 8085us
@@ -24,9 +26,40 @@ let init : WebPart =
   // creates a WebPart for the given implementation
   FableSuaveAdapter.webPartWithBuilderFor counterProcotol Route.builder
 
+let votes = ConcurrentBag<Vote>()
+
+let countVotes() =
+  let vs = Seq.toArray votes
+
+  let scores =
+    vs
+    |> Array.map (fun vote -> vote.Score)
+    |> Array.countBy id
+    |> Map.ofArray
+
+  let comments =
+    vs
+    |> Array.map (fun vote -> vote.Comment)
+
+  { Scores   = scores 
+    Comments = comments }
+
+let vote v : Async<VotingResults> =
+  async {
+    do votes.Add v
+    do! Async.Sleep 1000
+    return countVotes()
+  }
+
+let voting : WebPart =
+  let protocol =
+    { vote = vote }
+  FableSuaveAdapter.webPartWithBuilderFor protocol Route.builder
+
 let webPart =
   choose [
     init
+    voting
     Files.browseHome
   ]
 
